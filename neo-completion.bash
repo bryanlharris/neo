27b6e7a0ffd1ee1cec2af308e40dc217176a362d
Bryan Harris <bharris@neospire.net>
2009-04-23 17:36:42 -0500

    Make the search option variables globally exported
    I want to be able to have tab completion for every host in any of
    the pinky files and for any of the IP addresses in any of the pinky
    files.  So to do that one way is to grep and pipe to awk and do lots
    of other really intensive things on the file data until you get the
    hostnames or the IP addresses.
    
    Previously though, this was getting run every time you press the tab
    key, so it was just going really slowly to get the results up.  Now
    though, I'm putting them outside the function call so they just get
    run one time when the file gets sourced originally.  The variables
    are exported (yes it makes a mess), but to me it's worth it in order
    to have the extra speed every time I press tab.

--- neo-completion.bash
+++ neo-completion.bash
@@ -8,27 +8,22 @@ _neo()
 
     case "${prev}" in
         search)
-            search_opts="showpasswords --showpasswords $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
             COMPREPLY=( $(compgen -W "${search_opts}" -- ${cur}) )
             return 0
             ;;
         ssh)
-            ssh_opts="screen --screen $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
             COMPREPLY=( $(compgen -W "${ssh_opts}" -- ${cur}) )
             return 0
             ;;
         rdp)
-            rdp_opts="console --console $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
             COMPREPLY=( $(compgen -W "${rdp_opts}" -- ${cur}) )
             return 0
             ;;
         pix)
-            pix_opts="$(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
             COMPREPLY=( $(compgen -W "${pix_opts}" -- ${cur}) )
             return 0
             ;;
         --showpasswords)
-            showpasswords_opts="$(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
             COMPREPLY=( $(compgen -W "${showpasswords_opts}" -- ${cur}) )
             return 0
             ;;
@@ -39,4 +34,10 @@ _neo()
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0
 }
+
+export search_opts="showpasswords --showpasswords $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
+export ssh_opts="screen --screen $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
+export rdp_opts="console --console $(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
+export pix_opts="$(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
+export showpasswords_opts="$(neo search . | grep -v ^- | awk '{print $2}') $(neo search . | grep -v ^- | awk '{print $3}')"
 complete -F _neo neo
