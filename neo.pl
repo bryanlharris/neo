6126543eb65b37ada31477cefc540f8b74250098
Bryan Harris <bharris@bharris-laptop>
2009-04-25 17:17:01 -0500

    Fix a small issue
    When searching everything was fine, but when logging in, the neo.pl
    script was calling get_info function and was trying to grab the
    output of `neo.pl search --showpasswords query` which didn't work
    because neo.pl doesn't have a search sub any more.  So now instead
    it calls the correct script which is search.pl and gives the
    correct argument which is 192 since we're needing to get passwords.

--- neo.pl
+++ neo.pl
@@ -5,7 +5,7 @@ use Getopt::Lucid qw( :all );
 
 sub get_info {
     my ($query) = @_;
-    my @results = `neo.pl --showpasswords search $query`;
+    my @results = `search.pl 192 $query`;
     my $line;
     /\b$query\b/ and $line = $_ and last foreach (@results);
     my ($computername,$ip,$password) = (split / +/, $line)[1,2,3];
