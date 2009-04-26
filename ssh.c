661de070f7f34f930b7b537351a34cb72d6d2a6b
Bryan Harris <bharris@bharris-laptop>
2009-04-26 10:00:12 -0500

    Get rid of option loop in ssh.c, unneeded now
    There are not any options to ssh any more, although it's possible
    to add them back as long as the neo-ssh.exp script would be updated
    when it's added back.  Since I'm not searching for any options the
    loop is not needed anymore.

--- ssh.c
+++ ssh.c
@@ -6,35 +6,8 @@
 #include "usage.h"
 #include "check-ip.h"
 
-struct ssh_opt {
-    unsigned screen:1;
-};
-
 int cmd_ssh(int argc, char **argv, char **envp)
 {
-    int no_more_arg = 0;
-    char **dst, **src;
-    struct ssh_opt opt;
-
-    memset(&opt, 0, sizeof(opt));
-    
-    for (dst = src = &argv[1]; src < argc + argv; ) {
-        char *arg = *src++;
-        if (!no_more_arg) {
-            if (!strcmp("--", arg)) {
-                no_more_arg = 1;
-                *dst++ = arg;
-                continue;
-            }
-            if (!strcmp("--screen", arg) ||
-                !strcmp("screen", arg)) {
-                opt.screen = 1;
-                continue;
-            }
-        }
-        *dst++ = arg;
-    }
-
     char *ip = argv[1];
     if (check_ip(ip)) {
         printf("not a valid ip.\n");
