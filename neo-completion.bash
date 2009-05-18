4f1fa5b11d5058940db0ac7742d258456173309a
Bryan Harris <bharris@neospire.net>
2009-05-18 14:09:08 -0500

    Provide tab completion for uber logins
    When doing an uber login, you just login with a normal ssh command.
    This should allow the user to get a list of servers by pressing
    the tab key after typing ssh on a command line.

--- neo-completion.bash
+++ neo-completion.bash
@@ -35,6 +35,15 @@ _neo()
     return 0
 }
 
+_ssh()
+{
+    local cur
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=( $(compgen -W "${default_opts}" -- ${cur}) )
+    return 0
+}
+
 export default_opts="$(neo search . | awk '{print tolower($1) ".*" tolower($2) ".*" $3}')"
 export search_opts="showpasswords $default_opts"
 export ssh_opts="--ip $default_opts"
@@ -43,3 +52,4 @@ export pix_opts="$default_opts"
 export showpasswords_opts="$default_opts"
 export ip_opts="$(neo search . | awk '{print tolower($3)}')"
 complete -o default -F _neo neo
+complete -o default -F _ssh ssh
