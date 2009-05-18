67bf0f36862fcf91c0a8114a8c57fd44153ea219
Bryan Harris <bharris@neospire.net>
2009-05-18 14:17:03 -0500

    Add vncpass as a tab complete option

--- neo-completion.bash
+++ neo-completion.bash
@@ -4,7 +4,7 @@ _neo()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="help version search rdp ssh pix --help --version"
+    opts="help version search rdp ssh pix --help --version vncpass"
 
     case "${prev}" in
         search)
