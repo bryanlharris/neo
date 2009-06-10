772d5c19eed94a4d5d97f0e90b1874fa7cb491fa
Bryan Harris <bharris@neospire.net>
2009-06-09 20:18:56 -0500

    Clean up the neo-completion script
    This script creates the nice tab completion available for some of
    the neo commands.  In order to have tab completion for the differ-
    ent servers here, I would need to put those servers all into some
    variable accessible to the shell at the time the person is typing
    in the command.  Since there are so many servers this was creating
    a huge memory footprint in my shell's memory area.  I realized
    later on that using a bunch of memory for shell variables like I
    was doing would later cause problems for commands with really long
    argument lists.  Basically it looks like when you run a command
    all the arguments are limited to this same memory space, so if you
    have already used all the memory with your environment variables,
    as I had, then you won't be able to run simple commands like pipes
    and xargs type commands etc.  So in order to free up some memory
    I'm just going to greatly simplify this whole thing and clean it
    up a little bit.

--- neo-completion.bash
+++ neo-completion.bash
@@ -8,28 +8,24 @@ _neo()
 
     case "${prev}" in
         search)
-            COMPREPLY=( $(compgen -W "${search_opts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "showpasswords ${default_opts}" -- ${cur}) )
             return 0
             ;;
         showpasswords)
-            COMPREPLY=( $(compgen -W "${showpasswords_opts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${default_opts}" -- ${cur}) )
             return 0
             ;;
         ssh)
-            COMPREPLY=( $(compgen -W "${ssh_opts} ${hostname_opts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "--ip ${default_opts}" -- ${cur}) )
             return 0
             ;;
         rdp)
-            COMPREPLY=( $(compgen -W "${rdp_opts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${default_opts}" -- ${cur}) )
             return 0
             ;;
         pix)
-            COMPREPLY=( $(compgen -W "${pix_opts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${default_opts}" -- ${cur}) )
             return 0
-            ;;
-        --ip)
-            COMPREPLY=( $(compgen -W "${ip_opts}" -- ${cur}) )
-            return 0;
             ;;
         *)
             ;;
@@ -39,22 +35,5 @@ _neo()
     return 0
 }
 
-_ssh()
-{
-    local cur
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    COMPREPLY=( $(compgen -W "${default_opts} ${hostname_opts}" -- ${cur}) )
-    return 0
-}
-
-export default_opts="$(neo search . | awk '{print tolower($1) ".*" tolower($2) ".*" $3}')"
-export hostname_opts="$(neo search . | awk '{print tolower($2) ".*" $3}')"
-export search_opts="showpasswords $default_opts"
-export ssh_opts="--ip $default_opts"
-export rdp_opts="$default_opts"
-export pix_opts="$default_opts"
-export showpasswords_opts="$default_opts"
-export ip_opts="$(neo search . | awk '{print tolower($3)}')"
+export default_opts="$(neo search . | awk '{print tolower($2) ".*" $3}')"
 complete -o default -F _neo neo
-complete -o default -F _ssh ssh
